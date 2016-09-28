953f44a3524e8328181427e1393bd5071d8a064c
aa352160 <aa352160@126.com>
2016-09-28 19:18:16 +0800

    2016.9.28 update

--- app/src/main/java/com/example/jh352160/new_demo/test9/Test9.java
+++ app/src/main/java/com/example/jh352160/new_demo/test9/Test9.java
@@ -1,17 +1,28 @@
 package com.example.jh352160.new_demo.test9;
 
+import android.content.Intent;
+import android.graphics.Outline;
+import android.graphics.drawable.Drawable;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
+import android.view.ViewOutlineProvider;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.jh352160.new_demo.R;
 
-import java.util.concurrent.LinkedBlockingQueue;
+import java.util.Iterator;
+
+import rx.Observable;
+import rx.Subscriber;
+import rx.schedulers.Schedulers;
 
 /**
  * Created by jh352160 on 2016/9/27.
@@ -20,8 +31,9 @@ import java.util.concurrent.LinkedBlockingQueue;
 public class Test9 extends AppCompatActivity{
 
     private TextView textView1,textView2;
-    private Button button,button2;
+    private Button button,button2,button3;
     private ProgressBar progressBar;
+    private ImageView imageView;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -31,6 +43,9 @@ public class Test9 extends AppCompatActivity{
         textView1=(TextView)findViewById(R.id.textView1);
         textView2=(TextView)findViewById(R.id.textView2);
         button=(Button)findViewById(R.id.button1);
+        button2=(Button)findViewById(R.id.button2) ;
+        button3=(Button)findViewById(R.id.button3);
+        imageView=(ImageView)findViewById(R.id.imageView);
 
         button.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -43,7 +58,37 @@ public class Test9 extends AppCompatActivity{
         button2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                Observable.create(new Observable.OnSubscribe<Drawable>() {
+                    @Override
+                    public void call(Subscriber<? super Drawable> subscriber) {
+                        Drawable drawable=getTheme().getDrawable(R.drawable.category_image5);
+                        subscriber.onNext(drawable);
+                        subscriber.onCompleted();
+                    }
+                }).subscribe(new Subscriber<Drawable>() {
+                    @Override
+                    public void onCompleted() {
+                        Log.d("Message","ImageView is OK");
+                    }
 
+                    @Override
+                    public void onError(Throwable e) {
+                        Toast.makeText(Test9.this,"Error!!!!!!",Toast.LENGTH_SHORT).show();
+                    }
+
+                    @Override
+                    public void onNext(Drawable drawable) {
+                        imageView.setImageDrawable(drawable);
+                    }
+                });
+            }
+        });
+
+
+        button3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(Test9.this,Test9_2.class));
             }
         });
     }
@@ -80,6 +125,19 @@ public class Test9 extends AppCompatActivity{
         protected void onPostExecute(String s) {
             textView1.setText(s);
             super.onPostExecute(s);
+        }
+    }
+
+    class CustomIterator implements Iterator{
+
+        @Override
+        public boolean hasNext() {
+            return false;
+        }
+
+        @Override
+        public Object next() {
+            return null;
         }
     }
 }
---
+++ app/src/main/java/com/example/jh352160/new_demo/test9/Test9_2.java
@@ -0,0 +1,20 @@
+package com.example.jh352160.new_demo.test9;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import com.example.jh352160.new_demo.R;
+
+/**
+ * Created by jh352160 on 2016/9/28.
+ */
+
+public class Test9_2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.test9_2);
+    }
+}
